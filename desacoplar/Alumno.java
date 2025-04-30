846c047f733cf8f91b0c4fedba0f5f959af4fce3
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-04-30 16:16:49 +0000

    .

---
+++ desacoplar/Alumno.java
@@ -0,0 +1,57 @@
+package desacoplar;
+
+public class Alumno  {
+    //Atributo
+    private String nombre;
+    private int edad;
+    private double notaPromedio;
+    
+    //Constructor
+
+    public Alumno(String nombre, int edad, double notaPromedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.notaPromedio = notaPromedio;
+    }
+
+    //propiedades
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+
+
+    public void setNotaPromedio(double notaPromedio) {
+        this.notaPromedio = notaPromedio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + edad + ", " + notaPromedio ;
+    }
+
+
+    
+
+    
+    
+}
